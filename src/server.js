da69cd6224ae852dcbb632ccd42b1f80303175bd
Amitabh Dhiwal <amitabh@dhiwal.com>
2024-07-30 13:17:38 +0530

    change default port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app.js'
 
-const port = process.env.HTTP_PORT || 3030;
+const port = process.env.HTTP_PORT || 3000;
 const server = await app.listen(port)
 
 export default server;
