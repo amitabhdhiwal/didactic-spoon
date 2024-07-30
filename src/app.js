c3eb1b4ce6973ef4220c4fa276d0d3a53a0bd173
Amitabh Dhiwal <amitabh@dhiwal.com>
2024-07-30 13:15:52 +0530

    register http services

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv/config';
 import { feathers } from '@feathersjs/feathers'
 import express, { json, urlencoded, rest } from '@feathersjs/express'
 import { connect, disconnect, mongoose } from './lib/db.js';
+import {httpServices} from './services/index.js';
 
 const app = express(feathers())
 
@@ -22,5 +23,10 @@ app.use(urlencoded({ extended: true }))
 
 // Set up REST transport
 app.configure(rest())
+
+//register services
+httpServices.forEach(service => {
+    app.use(service.url, service.service);
+});
 
 export default app;
